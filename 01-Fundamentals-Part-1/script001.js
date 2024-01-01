d113f4d5e264a3b93548fc1cd297138e236e2624
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-01 12:48:57 -0300

    JavaScript - Values and Variables

---
+++ 01-Fundamentals-Part-1/script001.js
@@ -0,0 +1,20 @@
+// Values and Variables
+
+let myName = "Gabriel";
+let age = 21;
+console.log("Name:", myName, "Age: ", age);
+
+let person = "Gabriel Henrique";
+console.log(person);
+
+let degree = "Software Engineer";
+
+console.log("My name is", myName, "and a i study ", degree);
+
+let x = 2;
+let y = 5;
+
+let z = x + y;
+
+console.log(x + " + " + y + " = " + z);
+
