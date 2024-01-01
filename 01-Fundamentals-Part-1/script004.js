a20dc7c0b1ba46c309e47237edf98c86cfae9551
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-01 16:40:03 -0300

    JavaScript - Operators

---
+++ 01-Fundamentals-Part-1/script004.js
@@ -0,0 +1,37 @@
+//operators
+
+const now = 2024;
+const age = now - 2002;
+console.log(age);
+
+console.log(2 + 4);
+console.log(2 - 4);
+console.log(2 / 4);
+console.log(2 * 4);
+console.log(2 ** 4);
+
+const firstName = 'Gabriel';
+const secondName = 'Henrique';
+const fullName = firstName + secondName;
+
+console.log(firstName + " " + secondName); //Gabriel Henrique
+
+console.log(fullName); //GabrielHenrique
+
+let x = 10 + 5; //15
+console.log(x);
+
+x += 10; // x = x + 10 
+x -= 10; // 15
+x++; // x = x + 1 
+x--; // x = x -1
+console.log(x);
+
+
+
+//Comparison operators
+
+console.log(age > 0) // < , > , >= , <=, ==, ===
+
+
+
