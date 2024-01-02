c913f7eeaed8f34934d229d1d00b1ccfe1cb334f
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-02 16:28:14 -0300

    JavaScript - Dot vs Bracket Notation

---
+++ 02-Fundamentals-Part-2/script018.js
@@ -0,0 +1,52 @@
+'use strict';
+// Dot vs Bracket Notation
+
+const gabriel = {
+    firstName: 'Gabriel',
+    age: 2024 - 2002,
+    secondName: 'Henriuqe',
+    studentOf: 'Software Engineer',
+    birthYear: 2002,
+    friends: ['Miranda', 'Rodrigues']
+}
+
+console.log(gabriel);
+
+//to get the something from the object
+
+//Dot notation
+console.log(gabriel.firstName);
+console.log(gabriel.age);
+console.log(gabriel.studentOf);
+
+//Bracket Notation
+console.log(gabriel['firstName']);
+console.log(gabriel['age']);
+console.log(gabriel['studentOf']);
+
+const nameKey = gabriel;
+console.log(gabriel['firstName' + nameKey]);
+
+const interestedIn = prompt('firstName, age, studentOf:');
+
+if (gabriel[interestedIn]) {
+    console.log(gabriel[interestedIn]);
+    alert(gabriel[interestedIn]);
+} else {
+    console.log('Wrong Request');
+    alert('Wrong Request');
+}
+
+gabriel.age = 18;
+
+gabriel['firstName'] = 'GABRIEL';
+
+if (gabriel[interestedIn]) {
+    console.log(gabriel[interestedIn]);
+    alert(gabriel[interestedIn]);
+} else {
+    console.log('Wrong Request');
+    alert('Wrong Request');
+}
+
+console.log(`${gabriel.firstName} has ${gabriel.friends.length} friends and his best is  ${gabriel.friends[0]} `);
