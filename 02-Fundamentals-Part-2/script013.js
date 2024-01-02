33a6d9403ec29a00c72b154e937f2768dee16488
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-02 12:41:28 -0300

    JavaScript - Function Declaration vs Expressions

---
+++ 02-Fundamentals-Part-2/script013.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// you can call a function declaration before you declare it, even not it be a such good ideia in many cases.
+
+
+//function declaration
+function calcAge1(birthDay) {
+    return 2024 - birthDay;
+}
+
+const age1 = calcAge1(2002);
+console.log(age1);
+
+//funcion expression
+// expressions produce values
+
+// function without a nem which is called a Anonymous function
+const calcAge2 = function (birthDay) {
+    return 2024 - birthDay
+        ;
+}
+
+const age2 = calcAge2(2002);
+console.log(age2);
+
+// if you choose to use a function expression you will have to define the functions first at the top of the code and only yhen you can call them, so this make de code a little bit nicer and structed
