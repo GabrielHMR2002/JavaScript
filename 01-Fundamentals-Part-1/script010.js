68b9d4e953150c3b4b6c9b7758f0c162e558cb10
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-01 23:57:58 -0300

    JavaScript - Ternary Operator

---
+++ 01-Fundamentals-Part-1/script010.js
@@ -0,0 +1,15 @@
+// Ternary Operator
+
+let x = 0;
+
+x >= 4 ? x = 2 : x = -1;
+console.log(x);
+
+const myName = 'Gabriel';
+
+myName == 'Gabriel' ? console.log(true) : console.log(false);
+
+let k = 0;
+
+k === 1 ? k++ : k--;
+console.log(k);
