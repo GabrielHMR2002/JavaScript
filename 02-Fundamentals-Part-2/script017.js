f563eddde95877a5c971ac73d76a3cd7c0ee40ff
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-02 15:41:05 -0300

    JavaScript - Object

---
+++ 02-Fundamentals-Part-2/script017.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Objects
+
+//Array
+const gabrielArray = [
+    'Gabriel',
+    'Henrique',
+    'Software Engineer',
+    2002
+]
+
+console.log(gabrielArray); //  ['Gabriel', 'Henrique', 'Software Engineer', 2002]
+
+//Object
+const gabriel = {
+    firstName: 'Gabriel',
+    secondName: 'Henriuqe',
+    studentOf: 'Software Engineer',
+    birthYear: 2002
+
+}
+
+console.log(gabriel); // {firstName: 'Gabriel', secondName: 'Henriuqe', studentOf: 'Software Engineer', birthYear: 2002}
+
+// The big difference between Objects and Arrays is that an object the order of the values doesn't matter at all
+
+//In the array the orden means a lot because we want to acess it
