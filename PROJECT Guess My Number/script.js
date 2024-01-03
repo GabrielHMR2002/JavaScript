166c70ce60b8e212a566e8bd3c6d659e5c6d2c4e
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-03 18:06:58 -0300

    JavaScript - PROJECT Guess My Number - Logic Again

--- PROJECT Guess My Number/script.js
+++ PROJECT Guess My Number/script.js
@@ -14,7 +14,18 @@ console.log(document.querySelector('.guess').value);
 
 let score = 20;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.again').addEventListener('click', function () {
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1; 
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';        document.querySelector('.number').style.width = '15rem';
+})
 
 
 document.querySelector('.check').addEventListener('click', function () {
