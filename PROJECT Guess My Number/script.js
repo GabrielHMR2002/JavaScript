39401c2cc9b43df97449d923da478238863b2f0a
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-03 11:35:28 -0300

    JavaScript - PROJECT Guess My Number - Selecting and Manipulating Elements

---
+++ PROJECT Guess My Number/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 23;
+document.querySelector('.score').textContent = 10;
+
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
