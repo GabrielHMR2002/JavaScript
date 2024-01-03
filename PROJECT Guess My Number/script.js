b3c2a24a052747da1b2ac42bd7c99312846b5c06
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-03 12:00:16 -0300

    JavaScript - PROJECT Guess My Number - Handling Click Events

--- PROJECT Guess My Number/script.js
+++ PROJECT Guess My Number/script.js
@@ -1,12 +1,25 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number!';
 
-document.querySelector('.number').textContent = 23;
-document.querySelector('.score').textContent = 10;
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
 
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function(){
+const guess = Number(document.querySelector('.guess').value);
+console.log(guess, typeof guess);
+
+if(!guess){
+
+document.querySelector('.message').textContent = 'No number';
+
+}
+
+});
