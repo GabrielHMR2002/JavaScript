a880c84ba8f5467d7f27e7ef8af44d154a7c0bf6
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-06 00:39:34 -0300

    Working with classes

---
+++ PROJECT Modal Window/script.js
@@ -0,0 +1,29 @@
+"use strict";
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModel.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
