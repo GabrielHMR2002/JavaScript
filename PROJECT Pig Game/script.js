7ba95b80b9047fd0c25083bc528d3823820947b4
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-06 21:26:28 -0300

    PROJECT Pig Game -  Rolling the Dice

---
+++ PROJECT Pig Game/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('curent--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+
+    //Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice)
+
+    // Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //Check for rolled 1
+    if (dice != 1) {
+        //Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore;
+
+    } else {
+        //Switch to next player
+
+    }
+})
+
